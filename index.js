e1e28dc4db2cebf303e809c75f8483afe13b3783
ryba23 <ryba231@gmail.com>
2018-11-24 19:59:13 +0100

    Drawer

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
 import {Navigation} from "react-native-navigation";
+import {Dimensions} from 'react-native'
 import App from './App';
 
 Navigation.registerComponent('App',()=>App);
-
+/*
 Navigation.events().registerAppLaunchedListener(() => {
     Navigation.setRoot({
         root: {
             component: {
                 name: "App"
             }
+        }
+    });
+});*/
+
+const { width } = Dimensions.get('window');
+Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setDefaultOptions({
+        topBar: {
+            elevation: 0,
+            visible: false,
+            drawBehind: true,
+            animate: false,
+            buttonColor: 'white',
+            title: {
+                color: 'white',
+                alignment: 'center'
+            },
+            background: {
+                color: 'transparent'
+            }
+        }
+    });
+    Navigation.setRoot({
+        root: {
+            sideMenu: {
+                left: {
+                    component: {
+                        id: 'drawerId',
+                        name: 'App',
+                        fixedWidth: width
+                    }
+                },
+                center: {
+                    stack: {
+                        id: 'MAIN_STACK',
+                        children: [
+                            {
+                                component: {
+                                    name: 'App',
+                                }
+                            },
+                        ]
+                    }
+                },
+                right:{
+                    component:{
+                        id:'drawerId',
+                        name:'App',
+                        fixedWidth: width
+                    }
+                }
+            },
         }
     });
 });
