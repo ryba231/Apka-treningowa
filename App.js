b5c22820d71da359f4d3619dd1c8584487476359
ryba23 <ryba231@gmail.com>
2018-12-28 18:58:11 +0100

    Kalendarz testowy

--- App.js
+++ App.js
@@ -9,6 +9,8 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import CalendarStrip from 'react-native-calendar-strip';
+import moment from 'moment';
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,10 +22,24 @@ const instructions = Platform.select({
 type Props = {};
 export default class App extends Component<Props> {
     render() {
+        let datesWhitelist = [{
+            start: moment(),
+            end: moment().add(3, 'days')  // total 4 days enabled
+        }];
+        let datesBlacklist = [ moment().add(-1, 'days') ];
         return (
 
                 <LinearGradient colors={['#414867', '#2b324f',]} style={styles.linearGradient}>
                     <View style={styles.container}>
+                        <CalendarStrip
+                            daySelectionAnimation={{type: 'border', duration: 200, borderWidth: 1, borderHighlightColor: 'white'}}
+                            style={{height:150, paddingTop: 20, paddingBottom: 10}}
+                            disabledDateNameStyle={{color: '#8186A9'}}
+                            disabledDateNumberStyle={{color: '#8186A9'}}
+                            datesWhitelist={datesWhitelist}
+                            datesBlacklist={datesBlacklist}
+
+                        />
                         <Text style={styles.welcome}>Welcome to React Native!</Text>
                         <Text style={styles.instructions}>To get started, edit App.js</Text>
                         <Text style={styles.instructions}>{instructions}</Text>
@@ -37,8 +53,6 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
     },
     welcome: {
         fontSize: 20,
